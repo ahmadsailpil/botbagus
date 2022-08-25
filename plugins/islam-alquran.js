58286530fd5e01e71d036d27a997f144f31f9337
dawawasgg <106122054+dawawasgg@users.noreply.github.com>
2022-08-25 09:00:42 +0800

    Update islam-alquran.js

--- plugins/islam-alquran.js
+++ plugins/islam-alquran.js
@@ -1,20 +1,8 @@
-let fetch = require('node-fetch')
-
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!(args[0] || args[1])) throw `contoh:\n${usedPrefix + command} 1 2\n\nmaka hasilnya adalah surah Al-Fatihah ayat 2 beserta audionya, dan ayatnya 1 aja`
-
-    let res = await fetch(global.API('https://islamic-api-indonesia.herokuapp.com', '/api/data/quran', { surah: args[0], ayat: args[1] }))
-    let json = await res.json()
-    let mes = `${json.result.data.text.arab}
-    
-${json.result.data.translation.id}
-
-( Q.S ${json.result.data.surah.name.transliteration.id} : ${json.result.data.number.inSurah} )`.trim()
-    m.reply(mes)
-    conn.sendFile(m.chat, json.result.data.audio.primary, 'all.mp3', '', m, false, { mimetype: 'audio/mp4' })
-}
-
+let handler = async m => m.reply(`
+𝙈𝙀𝙉𝙐 𝙄𝙉𝙄 𝙇𝘼𝙂𝙄 𝙀𝙍𝙊𝙍 𝙔𝘼 𝙈𝘼𝘼𝙋
+`.trim()) // Tambah sendiri kalo mau
 handler.help = ['alquran *114 1*']
 handler.tags = ['islam']
 handler.command = /^(al)?quran$/i
+
 module.exports = handler
